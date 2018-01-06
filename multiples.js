cecfec05689b11786eb2fea09a42bbfa680fc97b
steveninouye <31404557+steveninouye@users.noreply.github.com>
2018-01-06 11:33:50 -1000

    Finished Multiples.js

--- multiples.js
+++ multiples.js
@@ -7,6 +7,12 @@ exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
   // do your work here
-
+  for (var i = 0; i < n; i++){
+    if(i % 3 === 0){
+      sum += i;
+    } else if (i % 5 === 0){
+      sum += i;
+    }
+  }
   return sum;
 };
